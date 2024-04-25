53cf2c7615802f1b5e88f872a77f3d228f7b8df3
Ukatu Ikechukwu <ukatuleonaldino@gmail.com>
2024-04-25 18:30:56 +0100

    Created a Reponsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { Button } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Hide, Show } from "@chakra-ui/react";
 
 function App() {
-
-  return (
+	return (
 		<>
-			<Button colorScheme="blue">Button</Button>
+			<Grid
+				templateAreas={{
+					base: `"nav" "main"`,
+					lg: `"nav nav" "aside main"`,
+				}}>
+				<GridItem area={"nav"} bg={"coral"}>
+					NavBar
+				</GridItem>
+				<Show above="lg">
+					<GridItem area={"aside"} bg={"green"}>
+						Aside
+					</GridItem>
+				</Show>
+				<GridItem area={"main"} bg={"blue"}>
+					Main
+				</GridItem>
+			</Grid>
 		</>
 	);
 }
 
-export default App
+export default App;
